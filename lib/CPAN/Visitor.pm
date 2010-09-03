68158cc4b7c12ec07f4358fefaea4ba36e0955a7
David Golden <dagolden@cpan.org>
2010-09-03 09:49:08 -0400

    Convert to dzil

--- lib/CPAN/Visitor.pm
+++ lib/CPAN/Visitor.pm
@@ -1,16 +1,9 @@
-# Copyright (c) 2010 by David Golden. All rights reserved.
-# Licensed under Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License was distributed with this file or you may obtain a
-# copy of the License from http://www.apache.org/licenses/LICENSE-2.0
-
-package CPAN::Visitor;
 use strict;
 use warnings;
-use autodie;
+package CPAN::Visitor;
+# ABSTRACT: Generic traversal of distributions in a CPAN repository
 
-our $VERSION = '0.001';
-$VERSION = eval $VERSION; ## no critic
+use autodie;
 
 use Archive::Extract ();
 use File::Find ();
@@ -288,14 +281,6 @@ __END__
 
 =begin wikidoc
 
-= NAME
-
-CPAN::Visitor - Generic traversal of distributions in a CPAN repository
-
-= VERSION
-
-This documentation describes version %%VERSION%%.
-
 = SYNOPSIS
 
     use CPAN::Visitor;
@@ -471,29 +456,6 @@ existing test-file that illustrates the bug or desired feature.
 
 * [App::CPAN::Mini::Visit]
 * [CPAN::Mini::Visit]
-
-= AUTHOR
-
-David A. Golden (DAGOLDEN)
-
-= COPYRIGHT AND LICENSE
-
-Copyright (c) 2010 by David A. Golden. All rights reserved.
-
-Licensed under Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License.
-A copy of the License was distributed with this file or you may obtain a
-copy of the License from http://www.apache.org/licenses/LICENSE-2.0
-
-Files produced as output though the use of this software, shall not be
-considered Derivative Works, but shall be considered the original work of the
-Licensor.
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 
 =end wikidoc
 
